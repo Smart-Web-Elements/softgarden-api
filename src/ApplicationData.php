acfa0cea63e43ab8f8e6fea99b1c14f007a8f780
Hamdan <h.al-rasbi@s-w-e.com>
2022-08-03 12:23:54 +0200

    setter and getter for ApplicationData

--- src/ApplicationData.php
+++ src/ApplicationData.php
@@ -16,57 +16,163 @@ class ApplicationData extends AbstractDataSet
 
     protected string $submittedOn = '';
 
-    protected string $username = '';
+    protected string $jobId = '';
 
-    protected string $password = '';
+    protected string $jobName = '';
+
+    protected string $firstName = '';
+
+    protected string $lastName = '';
+    
+    protected string $sex = '';
+
+    protected string $academictitle = '';
+
+    protected string $email = '';
+
+    protected string $externalProfileUrl = '';
 
     protected string $locale = 'de';
 
-    protected bool $internal = true;
+    protected string $zip = '';
 
-    protected bool $dataPrivacyAccepted = true;
+    protected string $city = '';
+    
+    protected string $country = '';
+
+    protected string $nationality = '';
+
+    protected string $phone = '';
+
+    protected string $mobilePhone = '';
+
+    protected string $dateofbirth = '';
+
+    protected string $coverLetterText = '';
+
+    protected string $region = '';
 
     /**
      * @inheritDoc
      */
-    public function getId()
+    public function getApplicationId()
     {
-        return $this->id;
+        return $this->applicationId;
     }
 
-    public function setId(string $id): void
+    public function setApplicationId(string $applicationId): void
     {
-        $this->id = $id;
+        $this->applicationId = $applicationId;
     }
 
-    public function getSalutaion()
+    public function getStatus()
     {
-        return $this->salutaion;
+        return $this->status;
     }
 
-    public function setSalutaion(string $salutation): void
+    public function setStatus(string $status): void
     {
-        $this->salutation = $salutation;
+        $this->status = $status;
     }
 
-    public function getFirstname()
+    public function getApplicationEditable()
     {
-        return $this->firstname;
+        return $this->applicationEditable;
     }
 
-    public function setFirstname(string $firstname): void
+    public function setApplicationEditable(Boolean $applicationEditable): void
     {
-        $this->firstname = $firstname;
+        $this->applicationEditable = $applicationEditable;
     }
 
-    public function getLastname()
+    public function getCreatedOn()
     {
-        return $this->lastname;
+        return $this->createdOn;
     }
 
-    public function setLastname(string $lastname): void
+    public function setCreatedOn(string $createdOn): void
     {
-        $this->lastname = $lastname;
+        $this->createdOn = $createdOn;
+    }
+
+    public function getLastChangedOn()
+    {
+        return $this->lastChangedOn;
+    }
+
+    public function setLastChangedOn(string $lastChangedOn): void
+    {
+        $this->lastChangedOn = $lastChangedOn;
+    }
+
+    public function getSubmittedOn()
+    {
+        return $this->submittedOn;
+    }
+
+    public function setSubmittedOn(string $submittedOn): void
+    {
+        $this->submittedOn = $submittedOn;
+    }
+
+    public function getJobId()
+    {
+        return $this->jobId;
+    }
+
+    public function setJobId(string $jobId): void
+    {
+        $this->jobId = $jobId;
+    }
+
+    public function getJobName()
+    {
+        return $this->jobName;
+    }
+
+    public function setJobName(string $jobName): void
+    {
+        $this->jobName = $jobName;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    public function setSex(string $sex): void
+    {
+        $this->sex = $sex;
+    }
+
+    public function getAcademictitle()
+    {
+        return $this->academictitle;
+    }
+
+    public function setAcademictitle(string $academictitle): void
+    {
+        $this->academictitle = $academictitle;
     }
 
     public function getEmail()
@@ -79,34 +185,14 @@ class ApplicationData extends AbstractDataSet
         $this->email = $email;
     }
 
-    public function getWorkEmail()
+    public function getExternalProfileUrl()
     {
-        return $this->workEmail;
+        return $this->externalProfileUrl;
     }
 
-    public function setWorkEmail(string $workEmail): void
+    public function setExternalProfileUrl(string $externalProfileUrl): void
     {
-        $this->workEmail = $workEmail;
-    }
-
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): void
-    {
-        $this->username = $username;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): void
-    {
-        $this->password = $password;
+        $this->externalProfileUrl = $externalProfileUrl;
     }
 
     public function getLocale()
@@ -119,23 +205,93 @@ class ApplicationData extends AbstractDataSet
         $this->locale = $locale;
     }
 
-    public function getinternal()
+    public function getZip()
     {
-        return $this->internal;
+        return $this->zip;
     }
 
-    public function setInternal(Boolean $internal): void
+    public function setZip(string $zip): void
     {
-        $this->internal = $internal;
+        $this->zip = $zip;
     }
 
-    public function getDataPrivacyAccepted()
+    public function getCity()
     {
-        return $this->dataPrivacyAccepted;
+        return $this->city;
     }
 
-    public function setDataPrivacyAccepted(Boolean $dataPrivacyAccepted): void
+    public function setCity(string $city): void
     {
-        $this->dataPrivacyAccepted = $dataPrivacyAccepted;
+        $this->city = $city;
+    }
+    
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    public function getNationality()
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(string $nationality): void
+    {
+        $this->nationality = $nationality;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    public function getMobilePhone()
+    {
+        return $this->mobilePhone;
+    }
+
+    public function setMobilePhone(string $mobilePhone): void
+    {
+        $this->mobilePhone = $mobilePhone;
+    }
+
+    public function getDateofbirth()
+    {
+        return $this->dateofbirth;
+    }
+
+    public function setDateofbirth(string $dateofbirth): void
+    {
+        $this->dateofbirth = $dateofbirth;
+    }
+
+    public function getCoverLetterText()
+    {
+        return $this->coverLetterText;
+    }
+
+    public function setCoverLetterText(string $coverLetterText): void
+    {
+        $this->coverLetterText = $coverLetterText;
+    }
+
+    public function getRegion()
+    {
+        return $this->region;
+    }
+
+    public function setRegion(string $region): void
+    {
+        $this->region = $region;
     }
 }
